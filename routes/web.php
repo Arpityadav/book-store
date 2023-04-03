02c0f4f7c641e95ba743a3f5db77831a6bdf273c
ArpitYadav <arpit.yadav1912@gmail.com>
2023-04-04 04:14:26 +0530

    Added laravel/ui package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Disable the registration
+Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
